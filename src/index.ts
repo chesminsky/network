7b0279812c395f5adbe1da87d4db8b8c9d841d69
Grigoriy Shishmintsev <gvshishm@mts.ru>
2019-08-07 18:24:57 +0300

    types

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import data from './data.json';
-const d3 = (<any>window).d3;
+import * as d3 from "d3";
+import { SimulationNodeDatum, SimulationLinkDatum } from 'd3';
+
+interface MyNode extends SimulationNodeDatum {
+    id: number;
+    name: string;
+}
 
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 30, bottom: 30, left: 40 },
+const margin = { top: 10, right: 30, bottom: 30, left: 40 },
     width = 400 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select('#my_dataviz')
+const svg = d3.select('#my_dataviz')
     .append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
@@ -16,26 +22,26 @@ var svg = d3.select('#my_dataviz')
         'translate(' + margin.left + ',' + margin.top + ')');
 
 // Initialize the links
-var link = svg
+const link = svg
     .selectAll('line')
-    .data(data.links)
+    .data<SimulationLinkDatum<MyNode>>(data.links)
     .enter()
     .append('line')
     .style('stroke', '#aaa')
 
 // Initialize the nodes
-var node = svg
+const node = svg
     .selectAll('circle')
-    .data(data.nodes)
+    .data<MyNode>(data.nodes)
     .enter()
     .append('circle')
     .attr('r', 20)
     .style('fill', '#69b3a2')
 
 // Let's list the force we wanna apply on the network
-var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
+d3.forceSimulation<MyNode>(data.nodes)                // Force algorithm is applied to data.nodes
     .force('link', d3.forceLink()                               // This force provides links between nodes
-        .id(function (d) { return d.id; })                     // This provide  the id of a node
+        .id((d: MyNode) => { return String(d.id); })                     // This provide  the id of a node
         .links(data.links)                                    // and this the list of links
     )
     .force('charge', d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
@@ -45,13 +51,25 @@ var simulation = d3.forceSimulation(data.nodes)                 // Force algorit
 // This function is run at each iteration of the force algorithm, updating the nodes position.
 function ticked() {
     link
-        .attr('x1', function (d) { return d.source.x; })
-        .attr('y1', function (d) { return d.source.y; })
-        .attr('x2', function (d) { return d.target.x; })
-        .attr('y2', function (d) { return d.target.y; });
+        .attr('x1', (d) => { 
+            return (<MyNode>d.source).x; 
+        })
+        .attr('y1', (d) => {
+            return (<MyNode>d.source).y;
+        })
+        .attr('x2', (d) => {
+            return (<MyNode>d.target).x;
+        })
+        .attr('y2', (d) => {
+            return (<MyNode>d.target).y;
+        });
 
     node
-        .attr('cx', function (d) { return d.x + 6; })
-        .attr('cy', function (d) { return d.y - 6; });
+        .attr('cx', (d) => {
+            return d.x + 6;
+        })
+        .attr('cy', (d) => {
+            return d.y - 6;
+        });
 }
 
